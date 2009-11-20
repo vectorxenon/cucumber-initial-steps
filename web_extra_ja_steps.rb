e1002f57f80665f5820350aab984f430d0278df2
YUKI Kaoru <k_yuki@bbmf.co.jp>
2009-11-20 12:41:53 +0900

    fixed ambiguous steps. fixed click_link step can run without "リンク"

--- web_extra_ja_steps.rb
+++ web_extra_ja_steps.rb
@@ -1,16 +1,16 @@
-Given /^言語は"(.*)"$/ do |lang|
+Given /^言語は"([^\"]*)"$/ do |lang|
   header("ACCEPT_LANGUAGE", lang)
 end
 
-When /^"(.*)"としてファイル"(.*)"をContent\-Type"(.*)"として添付する$/ do |field, path, content_type|
+When /^"([^\"]*)"としてファイル"([^\"]*)"をContent\-Type"([^\"]*)"として添付する$/ do |field, path, content_type|
   attach_file(field, path, content_type)
 end
 
-When /"(.*)"中の"(.*)"リンクをクリックする$/ do |element, label|
+When /"([^\"]*)"中の"([^\"]*)"リンクをクリックする$/ do |element, label|
   click_link_within(element, label)
 end
 
-When /^テーブル"(.*)"の"(.*)"行目の"(.*)"リンクをクリックする$/ do |css, nth, label|
+When /^テーブル"([^\"]*)"の"([^\"]*)"行目の"([^\"]*)"リンクをクリックする$/ do |css, nth, label|
   selector = "table.#{css} tbody tr:nth(#{nth})"
   click_link_within(selector, label)
 end
@@ -19,7 +19,7 @@ When /^デバッグのため$/ do
   save_and_open_page
 end
 
-Then /^テキストフィールドに"(.*)"と表示されていること$/ do |text|
+Then /^テキストフィールドに"([^\"]*)"と表示されていること$/ do |text|
   response.should have_tag("input[type=text][value=#{text}]")
 end
 
--- webrat_ja_steps.rb
+++ webrat_ja_steps.rb
@@ -5,19 +5,19 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "pat
 
 visit = lambda{|page_name| Given "I am on #{page_name}" }
 
-Given(/^"(.*)"ページを表示している$/, &visit)
-When(/^"(.*)"ページを表示する$/, &visit)
+Given(/^"([^\"]*)"ページを表示している$/, &visit)
+When(/^"([^\"]*)"ページを表示する$/, &visit)
 
-When /^"(.*)"ボタンをクリックする$/ do |button|
+When /^"([^\"]*)"ボタンをクリックする$/ do |button|
   When %Q(I press "#{button}")
 end
 
-When /^"(.*)"リンクをクリックする$/ do |link|
+When /^"([^\"]*)"(?:リンク)?をクリックする$/ do |link|
   When %Q(I follow "#{link}")
 end
 
 # Order is opposite from original (English) version
-When /^"(.*)"の"(.*)"リンクをクリックする$/ do |parent, link|
+When /^"([^\"]*)"の"([^\"]*)"(?:リンク)?をクリックする$/ do |parent, link|
   When %Q(I follow "#{link}" within "#{parent}")
 end
 
@@ -41,14 +41,14 @@ When /^以下の項目を入力する:$/ do |fields|
 end
 
 # opposite order from Engilsh one(original)
-When /^"(.*)"から"(.*)"を選択する$/ do |field, value|
+When /^"([^\"]*)"から"([^\"]*)"を選択する$/ do |field, value|
   When %Q(I select "#{value}" from "#{field}")
 end
 
 # Use this step in conjunction with Rail's datetime_select helper. For example:
 # When I select "December 25, 2008 10:00" as the date and time 
 # TODO 日本語が変だ
-When /^日時として"(.*)"を選択する$/ do |time|
+When /^日時として"([^\"]*)"を選択する$/ do |time|
   When %Q(I select "#{time}" as the date and time)
 end
 
@@ -61,7 +61,7 @@ end
 # The following steps would fill out the form:
 # When I select "November 23, 2004 11:20" as the "Preferred" date and time
 # And I select "November 25, 2004 10:30" as the "Alternative" date and time
-When /^"(.*)"の日時として"(.*)"を選択する$/ do |datetime_label, datetime|
+When /^"([^\"]*)"の日時として"([^\"]*)"を選択する$/ do |datetime_label, datetime|
   When %Q(I select "#{datetime_label}" as the "#{datetime}" date and time)
 end
 
@@ -69,53 +69,53 @@ end
 # When I select "2:20PM" as the time
 # Note: Rail's default time helper provides 24-hour time-- not 12 hour time. Webrat
 # will convert the 2:20PM to 14:20 and then select it. 
-When /^時間として"(.*)"を選択する$/ do |time|
+When /^時間として"([^\"]*)"を選択する$/ do |time|
   When %Q(I select "#{time}" as the time)
 end
 
 # Use this step when using multiple time_select helpers on a page or you want to
 # specify the name of the time on the form.  For example:
 # When I select "7:30AM" as the "Gym" time
-When /^"(.*)"の時間として"(.*)"を選択する$/ do |time_label, time|
+When /^"([^\"]*)"の時間として"([^\"]*)"を選択する$/ do |time_label, time|
   When %Q(I select "#{time_label}" as the "#{time}" time)
 end
 
 # Use this step in conjunction with Rail's date_select helper.  For example:
 # When I select "February 20, 1981" as the date
-When /^日付として"(.*)"を選択する$/ do |date|
+When /^日付として"([^\"]*)"を選択する$/ do |date|
   When %Q(I select "#{date}" as the date)
 end
 
 # Use this step when using multiple date_select helpers on one page or
 # you want to specify the name of the date on the form. For example:
 # When I select "April 26, 1982" as the "Date of Birth" date
-When /^"(.*)"の日付として"(.*)"を選択する$/ do |date_label, date|
+When /^"([^\"]*)"の日付として"([^\"]*)"を選択する$/ do |date_label, date|
   When %Q(I select "#{datetime_label}" as the "#{date}" date)
 end
 
-When /^"(.*)"をチェックする$/ do |field|
+When /^"([^\"]*)"をチェックする$/ do |field|
   When %Q(I check "#{field}")
 end
 
-When /^"(.*)"のチェックを外す$/ do |field|
+When /^"([^\"]*)"のチェックを外す$/ do |field|
   When %Q(I uncheck "#{field}")
 end
 
-When /^"(.*)"を選択する$/ do |field|
+When /^"([^\"]*)"を選択する$/ do |field|
   When %Q(I choose "#{field}")
 end
 
 # opposite order from Engilsh one(original)
-When /^"(.*)"としてファイル"(.*)"を選択する$/ do |field, path|
+When /^"([^\"]*)"としてファイル"([^\"]*)"を選択する$/ do |field, path|
   When %Q(I attach the file at "#{path}" to "#{field}")
 end
 
-Then /^"(.*)"と表示されていること$/ do |text|
+Then /^"([^\"]*)"と表示されていること$/ do |text|
   Then %Q(I should see "#{text}")
 end
 
 # opposite order from English definition
-Then /^"(.*)"に"(.*)"と表示されていること$/ do |selector, text|
+Then /^"([^\"]*)"に"([^\"]*)"と表示されていること$/ do |selector, text|
   Then %Q(I should see "#{text}" within "#{selector}")
 end
 
@@ -124,16 +124,16 @@ Then /^\/([^\/]*)\/と表示されていること$/ do |regexp|
 end
 
 # opposite order from English definition
-Then /^"(.*)"に\/([^\/]*)\/と表示されていること$/ do |selector, regexp|
+Then /^"([^\"]*)"に\/([^\/]*)\/と表示されていること$/ do |selector, regexp|
   Then %Q(I should see \/#{regexp}\/ within "#{selector}")
 end
 
-Then /^"(.*)"と表示されていないこと$/ do |text|
+Then /^"([^\"]*)"と表示されていないこと$/ do |text|
   Then %Q(I should not see "#{text}")
 end
 
 # opposite order from English definition
-Then /^"(.*)"に"(.*)"と表示されていないこと$/ do |selector, text|
+Then /^"([^\"]*)"に"([^\"]*)"と表示されていないこと$/ do |selector, text|
   Then %Q(I should not see "#{text}" within "#{selector}")
 end
 
@@ -142,27 +142,27 @@ Then /^\/([^\/]*)\/と表示されていないこと$/ do |regexp|
 end
 
 # opposite order from English definition
-Then /^"(.*)"に\/([^\/]*)\/と表示されていないこと$/ do |selector, regexp|
+Then /^"([^\"]*)"に\/([^\/]*)\/と表示されていないこと$/ do |selector, regexp|
   Then %Q(I should not see \/#{regexp}\/ within "#{selector}")
 end
 
-Then /^入力項目"(.*)"に"(.*)"と表示されていること$/ do |field, value|
+Then /^入力項目"([^\"]*)"に"([^\"]*)"と表示されていること$/ do |field, value|
   Then %Q(the "#{field}" field should contain "#{value}")
 end
 
-Then /^入力項目"(.*)"に"(.*)"と表示されていないこと$/ do |field, value|
+Then /^入力項目"([^\"]*)"に"([^\"]*)"と表示されていないこと$/ do |field, value|
   Then %Q(the "#{field}" field should not contain "#{value}")
 end
 
-Then /^"(.*)"がチェックされていること$/ do |label|
+Then /^"([^\"]*)"がチェックされていること$/ do |label|
   Then %Q(the "#{label}" checkbox should be checked)
 end
 
-Then /^"(.*)"がチェックされていないこと$/ do |label|
+Then /^"([^\"]*)"がチェックされていないこと$/ do |label|
   Then %Q(the "#{label}" checkbox should not be checked)
 end
 
-Then /^"(.*)"ページを表示していること$/ do |page_name|
+Then /^"([^\"]*)"ページを表示していること$/ do |page_name|
   Then %Q(I should be on #{page_name})
 end
 
